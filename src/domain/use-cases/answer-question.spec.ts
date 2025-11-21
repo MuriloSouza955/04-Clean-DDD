9a82c6d3389a519b856c7f28299b106f6cdb8f12
MuriloSouza955 <souza.murilo@gmail.com>
2025-11-21 15:14:15 -0300

    Dependenccias externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,17 @@
 import { expect, test } from 'vitest';
 import { AnswerQuestion } from './answer-question';
+import type { Answer } from '../entities/answer';
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestion();
+const fakeAnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestion(fakeAnswersRepository);
   
-  const answer = answerQuestion.execute({
+  const answer = await answerQuestion.execute({
     questionId: 'question-1',
     instructorId: 'instructor-1',
     content: 'Nova resposta'
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer";
+import type { AnswersRepository } from "../repositories/answers-repositories";
 
 interface AnswerQuestionRequest {
   instructorId: string;
@@ -7,8 +8,11 @@ interface AnswerQuestionRequest {
 }
 
 export class AnswerQuestion {
-  execute({ instructorId, questionId, content }: AnswerQuestionRequest) {
+  constructor(private answersRepository: AnswersRepository) {}
+  async execute({ instructorId, questionId, content }: AnswerQuestionRequest) {
     const answer = new Answer({ content, authorId: instructorId, questionId });
+
+    await this.answersRepository.create(answer);
 
     return answer;
   }
