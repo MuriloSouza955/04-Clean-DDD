659f6e859078bb124fbfd4758e41cb16ca43e3b3
MuriloSouza955 <souza.murilo@gmail.com>
2025-11-21 15:55:59 -0300

    Value Object

---
+++ src/domain/entities/value-object/slug.spec.ts
@@ -0,0 +1,8 @@
+import {expect, test} from 'vitest';
+import { Slug } from './slug';
+
+test('it shoud be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example question Title!')
+
+  expect(slug.value).toEqual('example-question-title');
+});
