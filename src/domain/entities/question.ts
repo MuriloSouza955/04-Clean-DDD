f4aea124c3184f715bb535819dded8c84930859b
MuriloSouza955 <souza.murilo@gmail.com>
2025-11-29 11:58:15 -0300

    Getters & Setters das entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,8 @@
 import { Slug } from "./value-object/slug";
 import { Entity } from "../../core/entities/entity";
-import type { UniqueEntityId } from "../../core/entities/unique-entity-id";
-import type { Optional } from "../../core/types/optional";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/types/optional";
+import dayjs = require("dayjs");
 
 interface QuestionProps {
   authorId: UniqueEntityId;
@@ -14,18 +15,77 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId(): UniqueEntityId | undefined {
+    return this.props.bestAnswerId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, "days") <= 3;
+  }
+
+  get excerpt() {
+    return this.content
+    .substring(0, 120)
+    .trimEnd()
+    .concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(value: string) {
+    this.props.content = value;
+    this.touch();
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
   static create(
-    props: Optional<QuestionProps, "createdAt">,
+    props: Optional<QuestionProps, "createdAt" | "slug">,
     id?: UniqueEntityId
   ) {
     const question = new Question(
       {
         ...props,
         createdAt: new Date(),
+        slug: props.slug ?? Slug.createFromText(props.title),
       },
       id
     );
-
     return question;
   }
 }
