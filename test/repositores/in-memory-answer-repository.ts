b15e20ec6ddc8ad315b7e2af1244f83c796cf877
MuriloSouza955 <souza.murilo@gmail.com>
2026-01-25 16:25:16 -0300

    Caso de uso: Listar respostas da pergunta

--- test/repositores/in-memory-answer-repository.ts
+++ test/repositores/in-memory-answer-repository.ts
@@ -1,8 +1,25 @@
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import type { PaginationParams } from '@/core/repositories/pagination-params'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
+  async findById(id: string){
+    const answer = this.items.find((item) => item.id.toString() === id)
+    
+    if (!answer) {
+      return null
+    }
+    return answer
+  }
+
+  async findManyByQuestionId(questionId: string, params: PaginationParams){
+    const answers = this.items.filter((item) => item.questionId.toString() === questionId)
+    .slice((params.page - 1) * 20, params.page * 20)
+    
+    return answers
+  }
 
   async create(answer: Answer) {
     this.items.push(answer)
@@ -17,12 +34,4 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     this.items[itemIndex] = answer
   }
 
-  async findById(id: string){
-    const answer = this.items.find((item) => item.id.toString() === id)
-    
-    if (!answer) {
-      return null
-    }
-    return answer
-  }
 }
